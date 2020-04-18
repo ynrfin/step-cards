93fdada3ff7a226b96bd6531614301e62d4fb39d
Yanuar Arifin <3326885+ynrfin@users.noreply.github.com>
2020-04-18 20:21:05 +0700

    parse html with bs4 and separate by hr tag for insert to card

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os, string
 import markdown
-from flask import Flask
+from flask import Flask, render_template
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
@@ -14,8 +15,13 @@ def view_article():
     filepath = scan_for_file('example.md')
     
     # Parse 
-    md_conten = parse_file_content(filepath)
-    return markdown.markdown(md_conten, extensions=['fenced_code'])
+    md_conten = read_file_to_string(filepath)
+
+    # Convert to HTML
+    generated_html=  markdown.markdown(md_conten, extensions=['fenced_code'])
+
+    # Separate HTML by <hr /> for card preparation
+    cards = assign_cards(generated_html)
 
 def scan_for_file(filename):
     search_path = 'articles'
@@ -24,11 +30,29 @@ def scan_for_file(filename):
             return os.path.join(dirpath, filename)
     return None
 
-def parse_file_content(filepath):
+def read_file_to_string(filepath):
     with open(filepath, 'r') as reader:
         content = reader.read()
-        print(content)
     return content
+
+def assign_cards(content):
+    soup = BeautifulSoup(content,'html.parser')
+
+    first_hr = soup.hr
+
+    next_elem = first_hr.next_sibling
+    cards = []
+    current_card = ""
+
+    for elem in first_hr.next_siblings:
+        current_card = current_card + str(elem)
+        if elem.name == "hr" :
+            cards.append(current_card)
+            current_card = ""
+
+    cards.append(current_card)
+
+    return cards
 
 if __name__ == "__main__":
     app.run(debug=True)
